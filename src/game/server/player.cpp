bb1b2f234b61f5b82c2d194e683fa7138ed5f591
Bamcane <diandaokui@qq.com>
2025-07-20 14:43:33 +0800

    Fix

--- src/game/server/player.cpp
+++ src/game/server/player.cpp
@@ -145,18 +145,18 @@ void CPlayer::PostTick()
 			{
 				GameServer()->SendChatTarget(m_ClientID, "Teleport request cancelled");
 				m_TeleportTimer = -1;
-
-				if(GameServer()->GameController()->IsInDarkMode())
-				{
-					GameServer()->CreatePlayerSpawn(m_ViewPos, GameWorld()->CmaskAllInWorld());
-					GameServer()->GameController()->OnPlayerDeathWhenDarkMode(m_ClientID);
-					return;
-				}
 			}
 		}
 	}
 
 	m_LastViewPos = m_ViewPos;
+
+	if(m_Team != TEAM_SPECTATORS && !m_pCharacter && GameServer()->GameController()->IsInDarkMode())
+	{
+		GameServer()->CreatePlayerSpawn(m_ViewPos, GameWorld()->CmaskAllInWorld());
+		GameServer()->GameController()->OnPlayerDeathWhenDarkMode(m_ClientID);
+		return;
+	}
 }
 
 void CPlayer::Snap(int SnappingClient)
